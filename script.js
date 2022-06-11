89409cf9eacd723315439cbf49797a122bee0419
witcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-06-12 00:26:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ for(let i = 1; i < 400; i++) {
     banner.innerHTML += "<div class='blocks'></div>";
     const duration = Math.random() * 10;
     blocks[i].style.animationDuration = 1 + duration + 's';
-    // blocks[i].style.animationDelay = `${i * 0.05}s`;
     blocks[i].style.animationDelay = duration + 's';
 }
 
