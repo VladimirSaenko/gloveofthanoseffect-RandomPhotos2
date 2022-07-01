52497df64a15d0a81eac23c955f81f5030f523e4
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-07-01 13:02:18 +0300

    fixed bug

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 
 "use strict";
 
-const banner = document.getElementsByClassName("banner")[0];
-const blocks = document.getElementsByClassName("blocks");
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+const section = document.querySelector('section');
+let time = 18000;
 
-for(let i = 1; i < 400; i++) {
-    banner.innerHTML += "<div class='blocks'></div>";
-    const duration = Math.random() * 10;
-    blocks[i].style.animationDuration = 1 + duration + 's';
-    blocks[i].style.animationDelay = duration + 's';
+for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < 10; j++) {
+        const block = document.createElement('div')
+        block.classList.add('blocks');
+        const up = 10 * j;
+        const left = 10 * i;
+        const bottom = 90 - 10 * j;
+        const right = 90 - 10 * i;
+        block.style.clipPath = `inset(${up}% ${left}% ${bottom}% ${right}%)`;
+        const duration = Math.random() * 5;
+        block.style.animationDuration = 2 + duration + 's';
+        block.style.animationDelay = duration + 's';
+        banner.append(block);
+    }
 }
 
-setTimeout(() => {
+setTimeout(() =>{
     location.reload();
-}, 23000);
+} , time);
